93d7f7168f464b0f6bf59d2beba91a65f39bb981
Luca Protelli <lucaprotelli23@icloud.com>
2022-12-16 18:55:52 +0100

    modified:   .gitignore modified:   server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -8,8 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/openai", require("./src/routes/openaiRoutes"));
 
 app.listen(port, () => {
-  console.log("Server started on port ${port}");
+  console.log(`Server started on port ${port}`);
 });
